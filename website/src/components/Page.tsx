84832c4b08c79536ba7d1b99b910ac9a4f045049
Gianluca Carucci <rucka@tiscalinet.it>
2022-04-24 15:34:23 +0200

    monolith

---
+++ website/src/components/Page.tsx
@@ -0,0 +1,17 @@
+import { Header } from '.'
+import React from 'react'
+import Head from 'next/head'
+import { GlobalStyle } from '../style';
+
+export function Page({ children, title }: { children: React.ReactElement; title?: string }) {
+  return (
+    <>
+      <Head>
+        <title>Pycon 2022{title ? ` - ${title}` : ''}</title>
+      </Head>
+      <GlobalStyle />
+      <Header />
+      {children}
+    </>
+  )
+}
